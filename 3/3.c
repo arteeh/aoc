a7574339272ba70f0d09a834babb1ecd445b04fc
Maarten de Jong <accounts@arteeh.com>
2020-12-04 16:34:46 +0100

    All of day 3 works

--- 3/3.c
+++ 3/3.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int t(int right, int down)
 {
-	FILE * input;
-	input = fopen("input","r");
-	
 	int trees = 0;
 	int pointer = 0;
+	char line[33];
 	
-	char line[64];
-	char current;
-	
+	FILE * input;
+	input = fopen("input","r");
 	while(fgets(line,sizeof(line),input))
 	{
-		current = line[pointer];
-		printf("%s",line);
+		//printf("%s",line);
+		//for(int i = pointer;i>0;i--) printf(" ");
+		//printf("^%i\n",pointer);
 		
-		for(int i = pointer;i>0;i--) printf(" ");
-		printf("^%i\n",pointer);
+		if(line[pointer] == '#') trees++;
 		
-		if(current == '#') trees++;
-		
-		pointer += 3;
+		pointer += right;
 		if(pointer > 30) pointer -= 31;
+		for(int i = 1; i < down; i++) fgets(line,sizeof(line),input);
 	}
+	fclose(input);
 	
-	printf("%i\n",trees);
+	printf("%i,%i:\t%i\n",right,down,trees);
 	return trees;
+}
+
+int main()
+{
+	long total = 1;
+	
+	total *= t(1,1);
+	total *= t(3,1);
+	total *= t(5,1);
+	total *= t(7,1);
+	total *= t(1,2);
+
+	printf("total:\t%li\n",total);
+	return total;
 }
